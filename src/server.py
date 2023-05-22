fc9cfece752f4970af7e45c78b6190f8f06d20c8
IvanIsak2000 <vingving648@gmail.com>
2023-05-22 20:41:46 +0300

    'update'

--- src/server.py
+++ src/server.py
@@ -3,9 +3,23 @@ import os
 import sys
 import time
 from datetime import datetime
+import logging 
+
+from genp import password_generation
+from PIL import Image   
+import qrcode
 import toml
+from rich.console import Console
+
+console = Console(highlight=False)
+logging.basicConfig(filename='server.log', level=logging.DEBUG, 
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger=logging.getLogger(__name__)
 
 
+def create_history_file():
+    with open('history.toml', 'w') as file:
+        file.write('Created!\n')
 
 def add_in_history(host: str, current_time: str, message: str):
 
@@ -21,59 +35,83 @@ def add_in_history(host: str, current_time: str, message: str):
 
 
 
-def connection_with_client(host, port):
+def await_connection(host: str, port: int, session_token: str):
     while True:
-
         try:
-            print('Connection waiting...')
-
-
+            console.print('[#9400D3]We are waiting for the connection...[#9400D3]')
             conn, address = server_socket.accept()
-            current_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-            print('connected with', address)
-
-            get_messege(conn, address, current_time)
+            console.print(f'[yellow]Connected with[yellow] {address}')
+            get_messege(conn, address, session_token)
 
         except socket.error as err:
             print(err)
-            pass
+            continue
 
 
 
-def get_messege(conn, address, current_time):
-        try:
+def get_messege(conn, address, session_token: str):
+        current_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+        client_token = conn.recv(1024).decode()
+
+        if client_token == session_token:
+            console.print('[green]The token is correct, we are waiting for the message')
+            response = 'True'
+            conn.send(response.encode())
+
             message = conn.recv(1024).decode()
-
             
 
-            print(f"[{(address[0])}] [{current_time}]: " + str(message))
-            print('_____________________________')
+            console.print(f"[{(address[0])}] [{current_time}]: " + str(message.split()[0]))
 
+            file = os.path.exists('history.txt')
+            if not file:
+                create_history_file()
+            add_in_history(host, current_time, message)  
+            logger.info(f'{address}: {message} {response}')             
 
-            add_in_history(host, current_time, message)
-        except socket.error as err:
-            print(err)
-            pass
-
-
+        else:
+            response = 'False'
+            conn.send(response.encode())
+            console.print('[red]The client entered the wrong session token. Connection closed')
+            logger.info(f'{address} {response}') 
+        conn.close()
+        print('_____________________________')
+    
+        
 if __name__ == '__main__':
+
+    try:
+        start = input('Start server? (enter/ Ctrl + C)')
+
+    except KeyboardInterrupt:
+        print('\nThe program is stopped !')
+        sys.exit()
 
     host =  socket.gethostbyname(socket.gethostname())
     port = 5000
+    password = (password_generation(True,False,False, 15))
+    session_token = host + str(port) + str(password)
+
+    print('Your host: ', host)
+    print('Your port: ', port)
+    print('Password: ', password)
+    print('Session token: ', host, port , password)
+    print('------------------------------')
+
+    qr_token = qrcode.make(f'{host} {port} {password}')
+    qr_token.save("session_token.png")
+    qr_token = Image.open('session_token.png')
+    qr_token.show() 
 
     server_socket = socket.socket(
-    family=socket.AF_INET, type=socket.SOCK_STREAM
-    )
+    family=socket.AF_INET, type=socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(2)
-    # server_socket.setblocking(False)
+
+    await_connection(host, port, session_token)
 
 
 
-    
-    connection_with_client(host, port)
-
-    # get_messege = get_messege(data[0], data[1], data[2])
 
 
-    # print(get_messege)
