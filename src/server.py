f49a1a12d6211c1fbeea9e55a213312633f591ab
IvanIsak2000 <vingving648@gmail.com>
2023-05-23 19:41:30 +0300

    'update_architecture'

--- src/server.py
+++ src/server.py
@@ -17,12 +17,18 @@ logging.basicConfig(filename='server.log', level=logging.DEBUG,
 logger=logging.getLogger(__name__)
 
 
-def create_history_file():
-    with open('history.toml', 'w') as file:
-        file.write('Created!\n')
+def check_token(conn: str, session_token: str) -> bool:
+    get_client_token = conn.recv(1024).decode()
+    if get_client_token == session_token:
+        return True
+    else:
+        return False
+
 
 def add_in_history(host: str, current_time: str, message: str):
-
+    if not os.path.isfile('history.toml'):
+        print('файла не было')
+        create_history_file()
     time_and_messege = {}
     full_session_data = {}
 
@@ -30,53 +36,51 @@ def add_in_history(host: str, current_time: str, message: str):
     full_session_data[host] = time_and_messege
 
     with open('history.toml', 'a') as file:
-        toml.dump(full_session_data, file)
+        toml.dump(full_session_data, file)    
 
-
+def create_history_file():
+    with open('history.toml', 'w') as file:
+        file.write('Created!\n')
 
 
 def await_connection(host: str, port: int, session_token: str):
+
     while True:
         try:
             console.print('[#9400D3]We are waiting for the connection...[#9400D3]')
             conn, address = server_socket.accept()
             console.print(f'[yellow]Connected with[yellow] {address}')
-            get_messege(conn, address, session_token)
+            send_response(conn, address, session_token)
 
         except socket.error as err:
             print(err)
             continue
 
+def send_response(conn: str, address: str, session_token: str) :
 
+    correcnt_token = check_token(conn, session_token)
+    if correcnt_token :      
+        response = 'True'
+        conn.send(response.encode())
+        console.print('[green]The token is correct, we are waiting for the message')  
+        get_message(conn, address)
 
-def get_messege(conn, address, session_token: str):
-        current_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-
-        client_token = conn.recv(1024).decode()
-
-        if client_token == session_token:
-            console.print('[green]The token is correct, we are waiting for the message')
-            response = 'True'
-            conn.send(response.encode())
-
-            message = conn.recv(1024).decode()
-            
-
-            console.print(f"[{(address[0])}] [{current_time}]: " + str(message.split()[0]))
-
-            file = os.path.exists('history.toml')
-            if not file:
-                create_history_file()
-            add_in_history(host, current_time, message)  
-            logger.info(f'{address}: {message} {response}')             
-
-        else:
-            response = 'False'
-            conn.send(response.encode())
-            console.print('[red]The client entered the wrong session token. Connection closed')
-            logger.info(f'{address} {response}') 
+    else:
+        response = 'False'
+        conn.send(response.encode())
+        console.print('[red]The client entered the wrong session token. Connection closed')
+        logger.info(f'{address}: {response}') 
         conn.close()
-        print('_____________________________')
+
+def get_message(conn, address)-> str:            
+    message = conn.recv(1024).decode()
+    current_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    console.print(f"[{(address[0])}] [{current_time}]: " + str(message.split()[0]))
+    add_in_history(address[0], current_time, message)  
+    logger.info(f'{address}: {message} {True}')             
+
+    conn.close()
+    print('_____________________________')
     
         
 if __name__ == '__main__':
