35e9d1d176b3c5129ab1a94a605d89d728b6089a
IvanIsak2000 <vingving648@gmail.com>
2023-07-10 14:22:18 +0300

    changed on `argparse`

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 import socket
 from datetime import datetime
 import logging
@@ -13,23 +14,19 @@ logging.basicConfig(filename='client.log', level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 
-client_socket: socket.socket   
-
-
 def connection_to_server_part(host: str, port: int) -> None:
-    global client_socket
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((host, port))
         console.print(f'[yellow]Your friend with IP {host} online!')
 
-        correct_response = send_and_get_response()
+        correct_response = send_and_get_response(client_socket)
 
         if correct_response:
             console.print('[green]Connection approved')  
             message = input("Enter your message: ")                      
-            send(message)
-            current_time = str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))  # не используется
+            send(client_socket, message)
+            current_time = str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
             console.print('[green]Message sent successfully!')
             logger.info(f'{host}: {message}')
 
@@ -45,43 +42,45 @@ def connection_to_server_part(host: str, port: int) -> None:
         print(e)
             
 
-def send_and_get_response() -> bool:
-    global client_socket
-
+def send_and_get_response(client_socket: socket.socket) -> bool:
     client_socket.send(token.encode())
     response = client_socket.recv(1024).decode()
-    if response == "True":
-        return True
-    else:
-        return False
+    return response == 'True'
+  
 
-
-def send(message: str) -> None:
-    global client_socket
-
+def send(client_socket: socket.socket, message: str) -> None:
     client_socket.send(message.encode())
 
 
 if __name__ == '__main__':
-    try:
-        action = sys.argv
 
-        if  action[1] == '--send':
-            token = str(action[2] + action[3] + action[4])
-            connection_to_server_part(str(action[2]), int(action[3]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a','--action', help='mode', default='send')
+    parser.add_argument('host', type=str, help='user host')
+    parser.add_argument('port', type=int, help='user port')
+    parser.add_argument('password', type=int, help='session password')
 
-        else:
-            input("""
-Error when entering a command!
-Available commands:
-python main.py --set (configures the server.py in a startup, so that the server is restarted when the PC is turned on)
-python main.py --send <host> <port> <password>""")
+    arg = parser.parse_args()
+    
+    if arg.action == 'send':
+        token = arg.host + str(arg.port) + str(arg.password)
+        connection_to_server_part(arg.host, arg.port)
 
-    except IndexError:
-        input("""
-The correct entry is:
-python main.py --set 
-python main.py --send <host> <port> <password>""")
+        # token = str(parser)
+        #     connection_to_server_part(str(action[2]), int(action[3]))
 
-    except Exception as err:
-        input(err)
+#         else:
+#             input("""
+
+# Available commands:
+# python main.py --set (configures the server.py in a startup, so that the server is restarted when the PC is turned on)
+# python main.py --send <host> <port> <password>""")
+
+#     except IndexError:
+#         input("""
+# The correct entry is:
+# python main.py --set 
+# python main.py --send <host> <port> <password>""")
+
+#     except Exception as err:
+        # input(err)
