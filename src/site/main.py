e8d09684cea34edd09bb9bdaefd9c908fd93e4e4
IvanIsak2000 <vingving648@gmail.com>
2023-08-25 19:48:49 +0300

    add `download` route

--- src/site/main.py
+++ src/site/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import send_file
 
 app = Flask(__name__)
 
@@ -7,5 +8,11 @@ app = Flask(__name__)
 def get_home_page():
     return render_template('home.html')
 
+
+@app.route('/download')
+def download_file():
+    path = 'cosmoterm'
+    return send_file(path, as_attachment=True)
+
 if __name__ == '__main__':
-     app.run(debug=True)
+    app.run(debug=True)
