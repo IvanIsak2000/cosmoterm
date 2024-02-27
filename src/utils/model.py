fd19cdc897d6728adda62db94979e53990e98d9e
IvanIsak2000 <vingving648@gmail.com>
2024-02-28 00:45:25 +0300

    add utulits for app

---
+++ src/utils/model.py
@@ -0,0 +1,31 @@
+import sqlalchemy
+from pydantic import BaseModel
+
+"""Sqlite бд для храния целей отправки"""
+
+class Target():
+    __tablename__ = 'target'
+
+    name: str
+    host: str
+    port: int
+
+
+
+def add_target(name, host, port):
+    pass
+
+def get_targets():
+    class Target(BaseModel):
+        id: int
+        name: str
+        host: str
+        port: int
+        
+        ...
+    
+    #  fake data are: 
+    return Target(id=0,name="jjodf", host='localhost', port=5008), Target(id=1,name="hjjhf", host='localhost', port=5008)
+
+def remove_target():
+    pass
